8767e2eb01a46fe5734cdf251eccf211712d984b
paul <dongpoding@gmail.com>
2021-01-04 14:54:40 +0800

    style

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Taro from '@tarojs/taro'
+import './style/taro-dates.scss'
 Taro.initPxTransform({ designWidth: 750, deviceRatio: {} })
 
 export { default as TaroDates } from './components/TaroDates'
